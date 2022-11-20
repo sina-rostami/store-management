f58ede22c66cb65844b18b8e8b10599259d81bac
sinarostami <Sinarostami4188.1@gmail.com>
2022-11-20 21:45:49 +0330

    Init backend branch

---
+++ backend/src/admin.py
@@ -0,0 +1,63 @@
+import json
+
+from customer import Customer
+from product import Product
+
+
+class Admin:
+    def __init__(self, id: int, name: str, customers: list, products: list) -> None:
+        self.id = id
+        self.name = name
+        self.customers = customers
+        self.products = products
+
+    def make_purchase_on_customer(self, customer_id: id, product_ids: list):
+        purchase_cost = 0
+        for product_id in product_ids:
+            for product in self.products:
+                if product.get_id() == product_id:
+                    purchase_cost += product.get_price()
+        for customer in self.customers:
+            if customer.get_id() == customer_id:
+                return customer.make_purchase(purchase_cost)
+        return None
+
+    def add_product(self, product: Product):
+        self.products.append(product)
+
+    def update_product(self, id : int, i_product: Product):
+        for product in self.products:
+            if product.get_id() == id:
+                product = i_product
+
+    def add_customer(self, customer: Customer):
+        self.customers.append(customer)
+
+
+    # create product handlers
+
+
+if __name__ == '__main__':
+    customer = Customer(12, 'ali', '09123213', 10000, 5000)
+    product = Product(11, 'sigar', 5000)
+    user = Admin(1, 'hamid', [customer], [product])
+
+    product1 = Product(13, 'sigar', 4000)
+
+    user.add_product(product1)
+
+    print(customer)
+    print(user.make_purchase_on_customer(12, [11]))
+    print(customer)
+    print(user.make_purchase_on_customer(12, [11]))
+    print(customer)
+    print(user.make_purchase_on_customer(12, [11]))
+    print(customer)
+    print(user.make_purchase_on_customer(12, [11]))
+    print(customer)
+    customer.deposit(10000)
+    print(customer)
+    print(user.make_purchase_on_customer(12, [13]))
+    print(customer)
+    customer.deposit(12000)
+    print(customer)
---
+++ backend/src/customer.py
@@ -0,0 +1,74 @@
+import json
+
+
+class Customer:
+    def __init__(self, id: int, name: str, phone_number: str, max_borrow: float, init_credit: float) -> None:
+        self.id = id
+        self.name = name
+        self.phone_number = phone_number
+        self.max_borrow = max_borrow
+        self.credit = init_credit
+        self.debt = float()
+
+    def make_purchase(self, purchase_cost: float) -> bool:
+        if self.credit + self.max_borrow - self.debt >= purchase_cost:
+            if self.credit >= purchase_cost:
+                self.credit -= purchase_cost
+                return True
+            else:
+                self.credit = 0
+                self.debt += purchase_cost
+                return True
+        else:
+            return False
+
+    def deposit(self, income: float()):
+        if self.debt != 0:
+            if self.debt >= income:
+                self.debt -= income
+                income = 0
+            else:
+                income -= self.debt
+                self.debt = 0
+        self.credit += income
+
+    def get_credit(self) -> float:
+        return self.credit
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_phone_number(self):
+        return self.phone_number
+
+    def get_debt(self):
+        return self.debt
+
+    def __str__(self) -> str:
+        return json.dumps({'id': self.id,
+                           'name': self.name,
+                           'phone_number': self.phone_number,
+                           'max_borrow': self.max_borrow,
+                           'credit': self.credit,
+                           'debt': self.debt})
+
+
+if __name__ == '__main__':
+    customer = Customer(12, 'ali', '09123213', 10000, 5000)
+
+    print(customer)
+    customer.make_purchase(5000)
+    print(customer)
+    customer.make_purchase(5000)
+    print(customer)
+    customer.make_purchase(5000)
+    print(customer)
+    customer.make_purchase(1000)
+    print(customer)
+    customer.deposit(1000)
+    print(customer)
+    customer.deposit(12000)
+    print(customer)
