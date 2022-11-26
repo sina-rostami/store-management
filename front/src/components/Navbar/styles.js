142b2644dddb5579140545b24e5b27ff0aec3c36
soroushazizi <soroushazizi@yahoo.com>
2022-11-26 23:51:40 +0330

    Init Navbar

---
+++ front/src/components/Navbar/styles.js
@@ -0,0 +1,17 @@
+import { createUseStyles } from 'react-jss'
+
+const styles = createUseStyles({
+  
+  navbarContainer : {
+    // Style 
+    '@media (min-width: 768px)': {
+
+    },
+
+    '@media (min-width: 1360px)': {
+
+    },
+  }
+})
+
+export default styles
