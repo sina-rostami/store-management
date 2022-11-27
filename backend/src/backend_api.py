0bcedca92f56c5bcad7d5e3fca3446a3fbdf6406
Alireza <alireza.dolatabadi.jc@gmail.com>
2022-11-27 03:56:40 +0330

    Added Security Part

--- backend/src/backend_api.py
+++ backend/src/backend_api.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from werkzeug.exceptions import BadRequest
 
 from backend import Backend
+from backend.src.security.security import token_required
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -23,6 +24,7 @@ backend = Backend()
 
 
 @app.route('/order', methods=['POST'])
+@token_required
 def place_order():
     try:
         did_success, message = backend.seller_manager.place_order(request.json)
