bdeee69ecd3a275aac6451c31ff372a37cf900a5
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-11-30 01:21:40 +0330

    seller panel added

---
+++ front/src/pages/SellerPanel/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import styles from './styles'
+
+const SellerPanel = () => {
+  const classes = styles()
+
+  return (
+    <div className={classes.sellerPanelRoot}>
+      <div className={classes.sellerItem}><Link to='/customers'>مدیریت مشتری‌ها</Link></div>
+      <div className={classes.sellerItem}><Link to='/bills'>مشاهده فاکتور‌های ثبت‌شده</Link></div>
+      <div className={classes.sellerItem}><Link>مدیریت محصولات</Link></div>
+    </div>
+  )
+}
+
+export default SellerPanel
