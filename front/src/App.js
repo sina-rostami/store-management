d1baf6bba7a93da0ed7889dcc0015b1c7a5cd3a9
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-11-29 17:11:40 +0330

    404 page added

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Ordering from './components/Ordering/index.jsx'
+import AdminPanel from './pages/AdminPanel/index.jsx'
 import BillsList from './pages/BillsList/index.jsx'
 import CustomersList from './pages/CustomersList/index.jsx'
+import NotFound from './pages/NotFound/index.jsx'
 import SellersList from './pages/SellersList/index.jsx'
 import SignIn from './pages/SignIn/index.jsx'
 
@@ -16,6 +18,9 @@ const App = () => {
       <Route path='/customers' element={<CustomersList />} />
       <Route path='/sellers' element={<SellersList />} />
       <Route path='/bills' element={<BillsList />} />
+      <Route path='/admin-panel' element={<AdminPanel />} />
+      <Route path='/not-found' element={<NotFound />} />
+      <Route path='*' element={<Navigate to='/not-found' />} />
     </Routes>
   )
 }
