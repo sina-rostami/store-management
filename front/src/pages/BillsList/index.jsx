1b40b9c32889025eefba156ab49c46ca223389fe
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-11-29 13:06:40 +0330

    header added to bills list

--- front/src/pages/BillsList/index.jsx
+++ front/src/pages/BillsList/index.jsx
@@ -11,6 +11,74 @@ const BillsList = () => {
       {
         id: 1,
         submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 2,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 3,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 4,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 5,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 6,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 7,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 8,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 9,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 10,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 11,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
+      },
+      {
+        id: 12,
+        submitDate: '1400/03/10',
+        sellerName: 'کبوان رضایی',
+        customerName: 'سینا رستمی',
       },
     ])
   }, [])
@@ -18,25 +86,24 @@ const BillsList = () => {
   return (
     <div className={classes.billsListRoot}>
       <h3 className={classes.pageTitle}>لیست فاکتور ها</h3>
+      <div className={classes.headerRow}>
+        <div className={classes.indexHeader}><span>شماره</span></div>
+        <div className={classes.sellerHeader}><span>فروشنده</span></div>
+        <div className={classes.customerHeader}><span>خریدار</span></div>
+        <div className={classes.idHeader}><span>شناسه</span></div>
+        <div className={classes.submitDateHeader}><span>تاریخ ثبت</span></div>
+      </div>
       {bills.length === 0
         ? <span>Loading ...</span>
         : bills.map((bill, index) => (
           <div className={classes.billRow} key={bill.id}>
             <div className={classes.indexContainer}><span>{index + 1}</span></div>
-            <div className={classes.imgContainer}>
-              {bill.imageUri
-                ? <img src="" alt="" />
-                : <div></div>
-              }
-            </div>
-            <div className={classes.nameContainer}>
-              <span>{bill.name}</span>
-            </div>
-            <div className={classes.submitDateContainer}>
-              <span>{bill.submitDate}</span>
-            </div>
+            <div className={classes.customerContainer}><span>{bill.customerName}</span></div>
+            <div className={classes.sellerContainer}><span>{bill.sellerName}</span></div>
+            <div className={classes.idContainer}><span>{bill.id}</span></div>
+            <div className={classes.submitDateContainer}><span>{bill.submitDate}</span></div>
             <div className={classes.seeMoreContainer}>
-              <img src="./asset/images/chevron-left.png" alt="" />
+              <img src="./asset/images/chevron-left.png" alt="مشاهده بیشتر" title="مشاهده بیشتر" />
             </div>
           </div>
         ))}
--- front/src/pages/BillsList/styles.js
+++ front/src/pages/BillsList/styles.js
@@ -6,13 +6,23 @@ const styles = createUseStyles({
     flexDirection: 'column',
     padding: [60, 30],
 
+    '& span': {
+      fontSize: 12,
+    },
+
     '@media (min-width: 768px)': {
       padding: [80, 50],
 
+      '& span': {
+        fontSize: 14,
+      },
     },
     '@media (min-width: 1360px)': {
       padding: [100, 60],
 
+      '& span': {
+        fontSize: 16,
+      },
     },
   },
 
@@ -26,6 +36,95 @@ const styles = createUseStyles({
     },
     '@media (min-width: 1360px)': {
       marginBottom: 50,
+
+    },
+  },
+
+  headerRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 40,
+    border: '1px solid black',
+    borderRadius: 5,
+    marginBottom: 15,
+    padding: [0, 10],
+
+    '@media (min-width: 768px)': {
+      height: 50,
+      marginBottom: 20,
+      padding: [0, 15],
+    },
+
+    '@media (min-width: 1360px)': {
+      height: 60,
+      marginBottom: 25,
+      padding: [0, 20],
+    },
+  },
+
+  indexHeader: {
+    display: 'flex',
+    width: '13%',
+
+    '@media (min-width: 768px)': {
+      width: '10%',
+    },
+
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  sellerHeader: {
+    display: 'flex',
+    width: '30%',
+
+    '@media (min-width: 768px)': {
+      width: '25%',
+
+    },
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  customerHeader: {
+    display: 'flex',
+    width: '23%',
+
+    '@media (min-width: 768px)': {
+      width: '21%',
+
+    },
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  idHeader: {
+    display: 'flex',
+    width: '15%',
+    fontSize: 12,
+
+    '@media (min-width: 768px)': {
+
+    },
+
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  submitDateHeader: {
+    display: 'none',
+
+    '@media (min-width: 768px)': {
+      display: 'flex',
+      width: '20%',
+    },
+
+    '@media (min-width: 1360px)': {
 
     },
   },
@@ -65,51 +164,46 @@ const styles = createUseStyles({
     },
   },
 
-  imgContainer: {
+  customerContainer: {
     display: 'flex',
-    width: '20%',
-
-    '& div': {
-      height: 25,
-      width: 25,
-      borderRadius: '50%',
-      backgroundColor: 'lightgray',
-    },
+    width: '30%',
 
     '@media (min-width: 768px)': {
 
-      '& div': {
-        height: 30,
-        width: 30,
-      },
     },
 
     '@media (min-width: 1360px)': {
 
-      '& div': {
-        height: 35,
-        width: 35,
-      },
     },
   },
 
-  nameContainer: {
+  sellerContainer: {
     display: 'flex',
-    width: '40%',
-    fontSize: 12,
+    width: '30%',
 
     '@media (min-width: 768px)': {
-      width: '30%',
-      fontSize: 14,
+
     },
 
     '@media (min-width: 1360px)': {
-      width: '30%',
-      fontSize: 16,
+
     },
   },
 
-  entryDateContainer: {
+  idContainer: {
+    display: 'flex',
+    width: '15%',
+
+    '@media (min-width: 768px)': {
+
+    },
+
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  submitDateContainer: {
     display: 'none',
 
     '@media (min-width: 768px)': {
@@ -125,7 +219,7 @@ const styles = createUseStyles({
   seeMoreContainer: {
     display: 'flex',
     justifyContent: 'flex-end',
-    width: '30%',
+    width: '15%',
     cursor: 'pointer',
 
     '& img': {
@@ -136,7 +230,7 @@ const styles = createUseStyles({
     },
 
     '@media (min-width: 768px)': {
-      width: '20%',
+      width: '15%',
 
       '& img': {
         height: 30,
@@ -145,7 +239,6 @@ const styles = createUseStyles({
     },
 
     '@media (min-width: 1360px)': {
-      width: '20%',
 
       '& img': {
         height: 35,
