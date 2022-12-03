4df2e49a6a91466838f1d8f5658a67f9903223c8
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-04 00:30:37 +0330

    react router added

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-// import SignIn from './components/SignIn/index.jsx'
+import { Routes, Route } from 'react-router-dom'
+
 import Ordering from './components/Ordering/index.jsx'
+import SignIn from './components/SignIn/index.jsx'
 
 const App = () => {
   return (
-    <>
-      {/* <SignIn /> */}
-      <Ordering />
-    </>
+    <Routes>
+      <Route index path='/' element={<Ordering />} />
+      <Route path="/sign-in" element={<SignIn />} />
+    </Routes>
   )
 }
 
