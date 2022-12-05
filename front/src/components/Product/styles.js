0aec2ea3bbb4f45f830726fe4c8bd2d827cd451f
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-05 03:51:40 +0330

    product component added

--- front/src/components/Product/styles.js
+++ front/src/components/Product/styles.js
@@ -1,65 +1,99 @@
 import { createUseStyles } from 'react-jss'
 
 const styles = createUseStyles({
-  productContainer: {
-    width: 200,
-    height: 200,
+  product: {
+    height: 170,
+    width: 170,
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     flexDirection: 'column',
-    justifyContent: 'space-around',
-
-    '@media (min-width: 768px)': {
-      width: 240,
-      height: 240,
-    },
-
-    '@media (min-width: 1360px)': {
-      width: 275,
-      height: 275,
-    },
-  },
-
-  productInfo: {
-    margin: [5, 10],
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'baseline',
-  },
-
-  cartButton: {
-    fontWeight: 700,
-    minWidth: 10,
-    height: 25,
-
-    '@media (min-width: 768px)': {
-      minWidth: 20,
-      height: 25,
-    },
-
-    '@media (min-width: 1360px)': {
-      minWidth: 30,
-      height: 35,
-    },
-  },
-
-  productImg: {
-    width: 100,
-    height: 100,
+    border: '1px solid black',
     borderRadius: 10,
-    objectFit: 'cover',
-    opacity: 0.8,
-    marginLeft: 'auto',
-    marginRight: 'auto',
+
+    '& img': {
+      height: 60,
+      width: 60,
+      marginBottom: 15,
+    },
+
+    '& span': {
+      fontSize: 14,
+    },
 
     '@media (min-width: 768px)': {
-      width: 150,
-      height: 150,
+      height: 250,
+      width: 250,
+
+      '& img': {
+        height: 100,
+        width: 100,
+        marginBottom: 25,
+      },
+
+      '& span': {
+        fontSize: 18,
+      },
     },
 
     '@media (min-width: 1360px)': {
-      width: 200,
-      height: 200,
+      height: 300,
+      width: 300,
+
+      '& img': {
+        height: 150,
+        width: 150,
+        marginBottom: 35,
+      },
+
+      '& span': {
+        fontSize: 22,
+      },
     },
+  },
+
+  name: {
+    fontSize: 14,
+
+    '@media (min-width: 768px)': {
+      fontSize: 18,
+    },
+
+    '@media (min-width: 1360px)': {
+      fontSize: 22,
+    },
+  },
+
+  price: {
+    fontSize: 14,
+
+    '@media (min-width: 768px)': {
+      fontSize: 18,
+    },
+
+    '@media (min-width: 1360px)': {
+      fontSize: 22,
+    },
+  },
+
+  firstRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginBottom: 20,
+  },
+
+  secondRow: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '60%',
+  },
+
+  btn: {
+    width: '30%',
+    height: 30,
+    borderRadius: 7,
   },
 })
 
