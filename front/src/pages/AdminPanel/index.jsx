04e6fea639bac6ae81b85dbb8d257e25f07bffaf
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-05 20:11:40 +0330

    admin panel page added

--- front/src/pages/AdminPanel/index.jsx
+++ front/src/pages/AdminPanel/index.jsx
@@ -9,10 +9,11 @@ const AdminPanel = () => {
 
   return (
     <div className={classes.adminPanelRoot}>
-      <div className={classes.adminItem}><Link to='/sellers'>مدیریت فروشندگان</Link></div>
-      <div className={classes.adminItem}><Link to='/customers'>مدیریت مشتری‌ها</Link></div>
+      <div className={classes.adminItem}><Link to='/sellers-mng'>مدیریت فروشندگان</Link></div>
+      <div className={classes.adminItem}><Link to='/customers-mng'>مدیریت مشتری‌ها</Link></div>
       <div className={classes.adminItem}><Link to='/bills'>مشاهده فاکتور‌های ثبت‌شده</Link></div>
       <div className={classes.adminItem}><Link to='/products'>مدیریت محصولات</Link></div>
+      <div className={classes.adminItem}><Link to='/ordering'>ثبت خرید</Link></div>
     </div>
   )
 }
