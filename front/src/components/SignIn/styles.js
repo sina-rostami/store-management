470376dc8c6c144509b9176c3a49175016e7652a
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-05 07:56:40 +0330

    sign in component added

---
+++ front/src/components/SignIn/styles.js
@@ -0,0 +1,15 @@
+import { createUseStyles } from 'react-jss'
+
+const styles = createUseStyles({
+  test: {
+
+    '@media (min-width: 768px)': {
+
+    },
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+})
+
+export default styles
