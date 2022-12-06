6217c8c1f96748f8889f7006f5e37ef442ba9a8a
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-07 02:12:14 +0330

    some routes added to App

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,19 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Ordering from './components/Ordering/index.jsx'
-import SignIn from './components/SignIn/index.jsx'
+import BillsList from './pages/BillsList/index.jsx'
+import CustomersList from './pages/CustomersList/index.jsx'
+import SellersList from './pages/SellersList/index.jsx'
+import SignIn from './pages/SignIn/index.jsx'
 
 const App = () => {
   return (
     <Routes>
       <Route index path='/' element={<Ordering />} />
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path='/customers' element={<CustomersList />} />
+      <Route path='/sellers' element={<SellersList />} />
+      <Route path='/bills' element={<BillsList />} />
     </Routes>
   )
 }
