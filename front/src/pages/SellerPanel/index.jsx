149981d7b04aeb150e5d4586657a1fac1f87ed46
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-06 20:41:40 +0330

    seller panel page added

--- front/src/pages/SellerPanel/index.jsx
+++ front/src/pages/SellerPanel/index.jsx
@@ -9,9 +9,10 @@ const SellerPanel = () => {
 
   return (
     <div className={classes.sellerPanelRoot}>
-      <div className={classes.sellerItem}><Link to='/customers'>مدیریت مشتری‌ها</Link></div>
+      <div className={classes.sellerItem}><Link to='/customers-mng'>مدیریت مشتری‌ها</Link></div>
       <div className={classes.sellerItem}><Link to='/bills'>مشاهده فاکتور‌های ثبت‌شده</Link></div>
       <div className={classes.sellerItem}><Link to='/products'>مدیریت محصولات</Link></div>
+      <div className={classes.sellerItem}><Link to='/ordering'>ثبت خرید</Link></div>
     </div>
   )
 }
