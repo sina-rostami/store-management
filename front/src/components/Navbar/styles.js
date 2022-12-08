d73844a31a5594fe1f3ca6974a9b1a88bfd28ca3
soroushazizi <soroushazizi@yahoo.com>
2022-12-08 13:31:40 +0330

    Styling Navbar

--- front/src/components/Navbar/styles.js
+++ front/src/components/Navbar/styles.js
@@ -1,6 +1,18 @@
 import { createUseStyles } from 'react-jss'
 
 const styles = createUseStyles({
+  
+  navbarContainer : {
+
+    width: '100%',
+    height: 60,
+    boxShadow: '0 1px 3px rgba(15,15,0.13)', 
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 1.5em',
+
+    //Tablet
+    '@media (min-width: 992px)': {
 
   navbarContainer: {
     width: '100%',
