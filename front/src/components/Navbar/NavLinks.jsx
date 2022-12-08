4a76009830360aacba34e7a63ec582733022ba63
soroushazizi <soroushazizi@yahoo.com>
2022-12-08 21:41:40 +0330

    Init Navbar Links

--- front/src/components/Navbar/NavLinks.jsx
+++ front/src/components/Navbar/NavLinks.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
+import React from "react"
 import styles from './styles'
+
 
 const NavLink = () => {
   const classes = styles()
-
+  
   return (
     <>
-      <div className={classes.navLinksContainer}/>
-      <div className={classes.linksWrapper}/>
-      <div className={classes.linkItem}/>
-      <div className={classes.link}/>
+    <div className={classes.navLinksContainer}/>
+    <div className={classes.linksWrapper}/>
+    <div className={classes.linkItem}/>
+    <div className={classes.link}/>
     </>
   )
 }
