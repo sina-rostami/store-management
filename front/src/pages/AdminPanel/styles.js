f84fc497ca2df7b5314f66d7942b3ce2a2a1a44d
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-09 13:17:53 +0330

    404 page added

---
+++ front/src/pages/AdminPanel/styles.js
@@ -0,0 +1,16 @@
+import { createUseStyles } from 'react-jss'
+
+const styles = createUseStyles({
+  adminPanelRoot: {
+
+    '@media (min-width: 768px)': {
+
+    },
+
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+})
+
+export default styles
