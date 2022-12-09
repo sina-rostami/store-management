be889be0ed6eb2da56e476dff44b32fc190a890e
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-09 16:56:42 +0330

    products page added

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import AdminPanel from './pages/AdminPanel/index.jsx'
 import BillsList from './pages/BillsList/index.jsx'
 import CustomersList from './pages/CustomersList/index.jsx'
 import NotFound from './pages/NotFound/index.jsx'
+import Products from './pages/Products/index.jsx'
 import SellerPanel from './pages/SellerPanel/index.jsx'
 import SellersList from './pages/SellersList/index.jsx'
 import SignIn from './pages/SignIn/index.jsx'
@@ -19,6 +20,7 @@ const App = () => {
       <Route path='/customers' element={<CustomersList />} />
       <Route path='/sellers' element={<SellersList />} />
       <Route path='/bills' element={<BillsList />} />
+      <Route path='/products' element={<Products />} />
       <Route path='/admin-panel' element={<AdminPanel />} />
       <Route path='/seller-panel' element={<SellerPanel />} />
       <Route path='/not-found' element={<NotFound />} />
