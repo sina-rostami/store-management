42137588a75a3816c9d14a1fd9015ae29fff1471
soroushazizi <soroushazizi@yahoo.com>
2022-12-10 13:11:05 +0330

    Init Navbar

---
+++ front/src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './styles'
+
+const Navbar = () => {
+  const classes = styles()
+
+  return (
+    <div className={classes.navbarContainer}>
+    </div>
+  )
+}
+
+export default Navbar
