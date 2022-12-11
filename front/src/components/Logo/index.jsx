a822ca9a317a08295035aa9dbcfa4e8e456107ec
soroushazizi <soroushazizi@yahoo.com>
2022-12-11 20:48:04 +0330

    Creating Logo Component

---
+++ front/src/components/Logo/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import LogoImg from '../../../public/asset/images/logo.png'
+
+
+import styles from './styles'
+
+const Logo = () => {
+  const classes = styles()
+  
+  return (
+    <div className={classes.logoWrapper}>
+    </div>
+    
+  )
+}
+
+export default Logo
