1d9679d27465585546f8505a96a22e21f144d7bb
soroushazizi <soroushazizi@yahoo.com>
2022-12-12 18:14:19 +0330

    Making the login and register button

---
+++ front/src/components/Navbar/Accessibility.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import styled from "styled-components"
+
+const AccessibilityContainer = styled.div`
+    display: flex;
+
+`;
+
+const LoginButton = styled.button`
+    border: 0;
+    outline: 0;
+    padding: 8px 1em;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+    border-radius: 20px;
+    transition: all 240ms ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        background-color #00c9ff;
+    }
+
+    &:not(:last-of-type){
+        margin-rigt: 7px;
+
+    }
+`;
+
+const RegisterButton = styled.button`
+    border: 0;
+    outline: 0;
+    padding: 8px 1em;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+    border-radius: 20px;
+    transition: all 240ms ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        background-color #00c9ff;
+    }
+
+    &:not(:last-of-type){
+        margin-rigt: 7px;
+        
+    }
+`;
+export function Accessibility(props){
+    return <AccessibilityContainer>
+        <RegisterButton>Register</RegisterButton>
+        <LoginButton>Login</LoginButton>
+    </AccessibilityContainer>
+}
