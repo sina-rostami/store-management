7adfb86c63aa674aebebedfc475326f25c7e04c8
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 13:02:06 +0330

    seller list page added

--- front/src/pages/SellersList/index.jsx
+++ front/src/pages/SellersList/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 
+import { useNavigate } from 'react-router-dom'
+
 import styles from './styles'
 
 const SellersList = () => {
   const [sellers, setSellers] = useState([])
   const classes = styles()
+  const navigate = useNavigate()
 
   useEffect(() => {
     setSellers([
@@ -85,7 +88,10 @@ const SellersList = () => {
 
   return (
     <div className={classes.sellersListRoot}>
-      <h3 className={classes.pageTitle}>لیست فروشندگان</h3>
+      <div className={classes.pageHeader}>
+        <img src="./asset/images/back.png" alt="بازگشت" title='بازگشت' onClick={() => navigate(-1)} />
+        <h3 className={classes.pageTitle}>لیست فروشندگان</h3>
+      </div>
       <div className={classes.headerRow}>
         <div className={classes.indexHeader}><span>شماره</span></div>
         <div className={classes.imgHeader}><span>عکس</span></div>
