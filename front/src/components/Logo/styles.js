d6be95f4d71a76b47791e9fc747ecf8fd714af2d
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 12:59:55 +0330

    logo added

---
+++ front/src/components/Logo/styles.js
@@ -0,0 +1,38 @@
+import { createUseStyles } from 'react-jss'
+
+const styles = createUseStyles({
+  logoInNav: {
+    marginTop: 60,
+
+    '@media (min-width: 768px)': {
+
+    },
+    '@media (min-width: 1360px)': {
+
+    },
+  },
+
+  logoWrapper : {
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  logoImg : {
+    width: 29,
+    height: 29,
+
+    '&img': {
+      width: '100%',
+      height: '100%',
+    },
+  },
+
+  logoText : {
+    fontSize: 16,
+    margin: 0,
+    marginLeft: 4,
+    fontWeight: 500,
+  },
+})
+
+export default styles
