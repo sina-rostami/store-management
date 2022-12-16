a5f2d7e4443777eca23882d66e20be139d65346d
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 12:58:32 +0330

    auth guard component added

---
+++ front/src/components/AuthGuard/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { useAuthState } from '../../context/index.js'
+import NotFound from '../../pages/NotFound/index.jsx'
+import Loading from '../Loading/index.jsx'
+
+const AuthGuard = ({ component, adminGuard = false, sellerGuard = false }) => {
+  const { authStatus, role } = useAuthState()
+
+  if (role === 'seller' && adminGuard) {
+    return <NotFound />
+  }
+  if (role === 'admin' && sellerGuard) {
+    return <NotFound />
+  }
+
+  if (authStatus === 'loggedIn') {
+    return component
+  } else if (authStatus === 'loggedOut') {
+    window.location.replace('/sign-in')
+  }
+
+  return <Loading />
+}
+
+export default AuthGuard
