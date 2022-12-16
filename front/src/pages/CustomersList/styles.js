95496a1d61ad1e238d7488a9561448284ee94b8e
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 13:01:12 +0330

    customers list page added

--- front/src/pages/CustomersList/styles.js
+++ front/src/pages/CustomersList/styles.js
@@ -14,15 +14,44 @@ const styles = createUseStyles({
     },
   },
 
+  pageHeader: {
+    position: 'relative',
+
+    '& img': {
+      position: 'absolute',
+      height: 20,
+      width: 20,
+      verticalAlign: 'middle',
+      cursor: 'pointer',
+    },
+
+    '@media (min-width: 768px)': {
+      '& img': {
+        height: 25,
+        width: 25,
+      },
+    },
+
+    '@media (min-width: 1360px)': {
+      '& img': {
+        height: 30,
+        width: 30,
+      },
+    },
+  },
+
   pageTitle: {
     textAlign: 'center',
     marginBottom: 30,
+    fontSize: 14,
 
     '@media (min-width: 768px)': {
       marginBottom: 40,
+      fontSize: 20,
     },
     '@media (min-width: 1360px)': {
       marginBottom: 50,
+      fontSize: 28,
     },
   },
 
