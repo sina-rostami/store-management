18f6be7fca180edb13700df1c5be8f76db016537
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 13:00:09 +0330

    product component added

--- front/src/components/Product/index.jsx
+++ front/src/components/Product/index.jsx
@@ -1,51 +1,27 @@
 import React from 'react'
 
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-
 import styles from './styles'
 
 function Product (props) {
-  const { product, handleCart } = props
+  const { product, handleCart, cart } = props
   const { name, price, id } = product
   const classes = styles()
 
+  const cartItem = cart.items.find(cartItem => cartItem.id === id)
+
   return (
-    <Box
-      className={classes.productContainer}
-      sx={{
-        // p: 1,
-        // m: 1,
-        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : 'grey.100'),
-        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
-        border: '1px solid',
-        borderColor: (theme) =>
-          theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-        borderRadius: 2,
-        fontSize: '0.875rem',
-        fontWeight: '700',
-      }}
-    >
-      <img className={classes.productImg} src={`./asset/images/${id}.png`} />
-      <div className={classes.productInfo}>
-        <span className={classes.productName}>{name}</span>
-        <Button
-          variant="contained"
-          className={classes.cartButton}
-          onClick={() => handleCart(id, 'add')}
-        >
-          +
-        </Button>
-        <Button
-          variant="contained"
-          className={classes.cartButton}
-          onClick={() => handleCart(id, 'remove')}
-        >
-          -
-        </Button>
-        <span className={classes.productPrice}>{price}</span>
+    <div className={classes.product} key={id}>
+      <img src={`./asset/images/${id}.png`} alt={name} title={name} />
+      <div className={classes.firstRow}>
+        <div className={classes.name}>{name}</div>
+        <div className={classes.price}>{price} تومان</div>
       </div>
-    </Box>
+      <div className={classes.secondRow}>
+        <button className={classes.btn} onClick={() => handleCart(id, 'add')}>+</button>
+        <span className={classes.qty}>{cartItem?.quantity ? cartItem.quantity : 0}</span>
+        <button className={classes.btn} onClick={() => handleCart(id, 'remove')}>-</button>
+      </div>
+    </div>
   )
 }
 
