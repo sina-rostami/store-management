03ef72ba338cfbfc1abe77fe18302776a5789db6
soroushazizi <soroushazizi@yahoo.com>
2022-12-16 16:47:26 +0330

    Making The Navbar Responsive

--- front/src/components/Navbar/accessibility.jsx
+++ front/src/components/Navbar/accessibility.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components"
 
 const AccessibilityContainer = styled.div`
     display: flex;
+    margin-left: 10px:
 `;
 
 const LoginButton = styled.button`
@@ -50,9 +51,32 @@ const RegisterButton = styled.button`
         margin-left: 7px;
     }
 `;
+
+const Exit = styled.button`
+    border: 0;
+    outline: 0;
+    padding: 8px 1em;
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+    border-radius: 20px;
+    background-color: #2980b9;
+    background-image: lenear-gradient(to right, transparent 0%, #00c9ff 100%);
+    transition: all 240ms ease-in-out;
+    cursor: pointer;
+
+    &:hover {
+        background-color #00c9ff;
+    }
+
+    &:not(:last-of-type){
+        margin-left: 7px;
+    }
+`;
 export function Accessibility(props){
     return <AccessibilityContainer>
-        <RegisterButton>Register</RegisterButton>
-        <LoginButton>Login</LoginButton>
+        <Exit>
+            خروج
+        </Exit>
     </AccessibilityContainer>
 }
