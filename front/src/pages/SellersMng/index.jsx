012bc3654f0f11f94323e75ca4f8285e60fe7124
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 13:02:13 +0330

    sign in page added

---
+++ front/src/pages/SellersMng/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import styles from './styles'
+
+const SellersMng = () => {
+  const classes = styles()
+
+  return (
+    <div className={classes.sellersMngRoot}>
+      <div className={classes.sellerItem}><Link to='/sellers'>لیست فروشندگان</Link></div>
+      <div className={classes.sellerItem}><Link to='/add-seller'>افزودن فروشنده</Link></div>
+      <div className={classes.sellerItem}><Link to='/remove-seller'>حذف فروشنده</Link></div>
+    </div>
+  )
+}
+
+export default SellersMng
