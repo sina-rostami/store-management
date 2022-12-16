50b2bf413d12c2c98d4b5a739bd9242aea48c5b1
soroushazizi <soroushazizi@yahoo.com>
2022-12-16 13:13:33 +0330

    Doing Some Navbar Changes

--- front/src/components/Navbar/index.jsx
+++ front/src/components/Navbar/index.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 
-import styles from './styles'
+import styled from "styled-components";
 
-const Navbar = () => {
-  const classes = styles()
+const navBarContainer = styled.div`
+  width: 100%;
+  height: 60;
+  boxShadow: 0 1px 3px rgba(15,15,0.13); 
+  display: flex;
+  alignItems: center;
+  padding: 0 1.5em;
+`;
 
-  return (
-    <div className={classes.navbarContainer}>
-    </div>
-  )
+const LeftSection = styled.div`
+  display: flex;
+`;
+
+const MiddleSection = styled.div`
+  display: flex;
+  flex: 2;
+  height: 100%;
+  justifyContent: center;
+`;
+
+const RightSection = styled.div`
+  display: flex;
+`;
+
+export function Navbar(props){
+  return <navBarContainer>
+    <LeftSection></LeftSection>
+    <MiddleSection></MiddleSection>
+    <RightSection></RightSection>
+  </navBarContainer>
 }
-
-export default Navbar
