ea034fe0c8d347d06ee5cf7c2fc8799e2f338789
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-12-16 13:01:28 +0330

    customer management page added

---
+++ front/src/pages/CustomersMng/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import styles from './styles'
+
+const CustomersMng = () => {
+  const classes = styles()
+
+  return (
+    <div className={classes.customersMngRoot}>
+      <div className={classes.customerItem}><Link to='/customers'>لیست مشتری‌ها</Link></div>
+      <div className={classes.customerItem}><Link to='/add-customer'>افزودن مشتری</Link></div>
+      <div className={classes.customerItem}><Link to='/remove-customer'>حذف مشتری</Link></div>
+    </div>
+  )
+}
+
+export default CustomersMng
