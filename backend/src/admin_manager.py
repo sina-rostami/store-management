024088ffa85954fac5d2775f7a79c10aa17f6938
Alireza <alireza.dolatabadi.jc@gmail.com>
2022-12-28 10:20:33 +0330

    Change Admin Username

--- backend/src/admin_manager.py
+++ backend/src/admin_manager.py
@@ -10,13 +10,8 @@ class AdminManager:
     def edit_admin(self, data):
         username, password = data.get('username'), data.get('password')
 
-        default_rows = {'admin_username': username, 'admin_password': generate_password_hash(password)}
-
-        for key, value in default_rows.items():
-            obj = self.database_session.query(Scratch).filter_by(key=key).first()
-            if self.database_session.query(Scratch).filter_by(key=key).first():
-                self.database_session.delete(obj)
-            self.database_session.add(Scratch(key, value))
+        obj = self.database_session.query(Scratch).filter_by(key='admin_username').first()
+        obj.value = username
 
         self.database_session.commit()
 
