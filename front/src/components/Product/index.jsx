69e3a6b69fff040443ed60d0f4b268bcd44e81fe
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2023-01-01 21:52:03 +0330

    checking stock number added

--- front/src/components/Product/index.jsx
+++ front/src/components/Product/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './styles'
 import dltf from '../../utilities/dltf.js'
+import cx from 'classnames'
 
 function Product (props) {
   const { product, handleCart, cart } = props
-  const { name, price, id } = product
+  const { name, price, id, stock_number } = product
+  const [stockNumber, setStockNumber] = useState(stock_number)
   const classes = styles()
 
   const cartItem = cart.items.find(cartItem => cartItem.id === id)
+
+  const handleStockNumber = (action) => {
+    if (action === 'add') {
+      setStockNumber(stockNumber + 1)
+    } else if (action === 'sub') {
+      setStockNumber(stockNumber - 1)
+    }
+  }
 
   return (
     <div className={classes.product} key={id}>
@@ -17,10 +27,27 @@ function Product (props) {
         <div className={classes.name}>{name}</div>
         <div className={classes.price}>{dltf(price)} تومان</div>
       </div>
+      <div className={cx({[classes.stockRow]: true, [classes.zeroStock]: !!!stockNumber})}>
+        <span>موجودی انبار: {dltf(stockNumber)}</span>
+      </div>
       <div className={classes.secondRow}>
-        <button className={classes.btn} onClick={() => handleCart(id, 'add')}>+</button>
-        <span className={classes.qty}>{cartItem?.quantity ? dltf(cartItem.quantity) : dltf(0)}</span>
-        <button className={classes.btn} onClick={() => handleCart(id, 'remove')}>-</button>
+        <button
+          className={cx({[classes.btn]: true, [classes.disabledBtn]: !!!stockNumber})}
+          onClick={!!stockNumber ? () => {handleCart(id, 'add'); handleStockNumber('sub')} : null}
+        >
+          +
+        </button>
+        <span
+          className={classes.qty}
+        >
+          {cartItem?.quantity ? dltf(cartItem.quantity) : dltf(0)}
+        </span>
+        <button
+          className={cx({[classes.btn]: true, [classes.disabledBtn]: !!!cartItem?.quantity})}
+          onClick={cartItem?.quantity ? () => {handleCart(id, 'remove'); handleStockNumber('add')} : null}
+        >
+          -
+        </button>
       </div>
     </div>
   )
