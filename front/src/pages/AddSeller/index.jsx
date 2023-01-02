737fd482502b70f6461431d8a8821d5350f80f60
soroushazizi <soroushazizi@yahoo.com>
2023-01-02 21:34:39 +0330

    Some Changes

--- front/src/pages/AddSeller/index.jsx
+++ front/src/pages/AddSeller/index.jsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import addSeller from '../../services/addSeller.js'
 import { useNavigate } from 'react-router-dom'
-import ImageUpload from '../../components/ImageUpload/ImageUpload'
+import ImageUpload from '../../components/ImageUpload/index.jsx'
 
 const AddSeller = () => {
   const classes = styles()
