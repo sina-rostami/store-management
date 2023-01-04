c5f490fc7e71eb62db288324f48ac44fe93097b4
soroushazizi <soroushazizi@yahoo.com>
2023-01-05 02:45:29 +0330

    style basic dropDownMenu

--- front/src/components/DropDrownMenu/index.jsx
+++ front/src/components/DropDrownMenu/index.jsx
@@ -1,7 +1,26 @@
 import { Button } from '@mui/material';
 import React from 'react';
+import styles from './styles';
 
 function DropDownMenu(props){
+
+    const classes = styles()
+
+    function DropDownItem(props){
+        return(
+            <h1 href="#" className='menu-item'>
+                <span className='icon-button'> {props.leftIcon} </span>
+                {props.children}
+                <span className='icon-right'> {props.rightIcon} </span>
+            </h1>
+        )
+    }
+    return(
+        <div className= {classes.dropDownMenuContainer}>
+            <DropDownItem className={classes.menuItem}>My Profile</DropDownItem>
+            <DropDownItem>Exit</DropDownItem>
+        </div>
+    )
 }
 
 export default DropDownMenu
