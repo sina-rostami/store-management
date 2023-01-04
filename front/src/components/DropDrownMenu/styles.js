0cb7f9cb16024a8b1a9393807ac74ff488295646
soroushazizi <soroushazizi@yahoo.com>
2023-01-05 02:45:29 +0330

    style basic dropDownMenu

--- front/src/components/DropDrownMenu/styles.js
+++ front/src/components/DropDrownMenu/styles.js
@@ -1,7 +1,26 @@
+import { border } from '@mui/system'
 import { createUseStyles } from 'react-jss'
 
 const styles = createUseStyles({
+    dropDownMenuContainer: {
+        position: 'absolute',
+        display: 'flex',
+        top: 100,
+        width: 150,
+        height: 150,
+        padding: '1rem',
+        overflow: 'hidden',
+        marginBottom: 30,
+        marginTop: 10,
+        backgroundColor: 'white',
+    },
 
+    menuItem: {
+        height: 50,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0.5rem',
+    }
 })
 
 export default styles
