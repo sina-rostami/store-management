df29554e0fbca392b813acc2fbe61afa1581964e
soroushazizi <soroushazizi@yahoo.com>
2023-01-05 19:37:07 +0330

    make some minor changes

--- front/src/components/DropDrownMenu/index.jsx
+++ front/src/components/DropDrownMenu/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles';
 import { useAuthDispatch } from '../../context/index.js'
-
+import noop from 'lodash/noop'
 
 function DropDownMenu(props){
 
@@ -15,21 +15,21 @@ function DropDownMenu(props){
         authDispatch({ type: 'logout' })
     }
 
-    function DropDownItem(props){
+    function DropDownItem({ clickHandler, children }){
         return(
-            <h1 href="#" className='menu-item'>
-                <span className='icon-button'> {props.leftIcon} </span>
-                {props.children}
-                <span className='icon-right'> {props.rightIcon} </span>
-            </h1>
+            <li onClick={clickHandler?clickHandler:noop}>
+                {children}
+            </li>
         )
     }
     return(
         <div className= {classes.dropDownMenuContainer}>
+            <ul className={classes.items}>
             <DropDownItem className={classes.menuItem}>My Profile</DropDownItem>
-            <DropDownItem onClick={logoutHandler}>
+            <DropDownItem clickHandler={logoutHandler} className={classes.menuItem}>
                     خروج            
             </DropDownItem>
+            </ul>
         </div>
     )
 }
