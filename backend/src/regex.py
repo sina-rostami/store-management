30974c1ad8af0f3fc0a02584a0ba90145c38cce6
Sina Rostami <Sinarostami4188.1@gmail.com>
2023-01-06 14:32:31 +0330

    Improve check fields

--- backend/src/regex.py
+++ backend/src/regex.py
@@ -3,11 +3,11 @@ patterns = {
     'phone_number': r'^09[0|1|2|3][0-9]{8}$',
     'name': r'^[^!@#$%^&*0-9]{2,20}$',
     'username': r'^[A-Za-z_0-9]{4,15}$',
-    'stock_number': r'\d',
-    'credit': r'\d',
-    'price': r'\d',
-    'category_id': r'\d',
-    'customer_id': r'\d',
-    'seller_id': r'\d',
-    'balance': r'\d'
+    'stock_number': r'^\d+$',
+    'credit': r'^\d+(.\d+|)$',
+    'price': r'^\d+(.\d+|)$',
+    'category_id': r'^\d+$',
+    'customer_id': r'^\d+$',
+    'seller_id': r'^\d+$',
+    'balance': r'^\d+(.\d+|)$'
 }
