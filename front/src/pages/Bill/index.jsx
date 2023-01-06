24b2b845aaf9d11f869358bdb2b36ecee8ae5a79
soroushazizi <soroushazizi@yahoo.com>
2023-01-06 13:51:32 +0330

    minor changes on bill

--- front/src/pages/Bill/index.jsx
+++ front/src/pages/Bill/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./style";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import Cart from '../../components/Cart/index.jsx'
+import getBill from '../../services/getBill.js'
+
 
 function Bill (props) {
+    
+    const [bill, setBill] = useState(null)
     const classes = styles()
     const navigate = useNavigate()
+    const location = useLocation()
+    console.log(bill)
+
+    useEffect(() => {
+        getBill(location.state.id).then(res => setBill(res))
+    },[])
 
     return (
         <div className={classes.pageHeader}>
@@ -12,12 +23,13 @@ function Bill (props) {
             <h3 className={classes.pageTitle}>فاکتور</h3>
         <div>
             <ul>
-                <h4 className={classes.sellerName}>فروشنده</h4>
-                <h4 className={classes.customerName}>خریدار</h4>
-                <h4 className={classes.id}>شناسه خرید</h4>
-                <h4 className={classes.purchaseDate}>تاریخ خرید</h4>
-                <h4 className={classes.shoppingCart}>سبد خرید</h4>
+                <li className={classes.sellerName}>فروشنده</li>
+                <li className={classes.customerName}>خریدار</li>
+                <li className={classes.id}>شناسه خرید</li>
+                <li className={classes.purchaseDate}>تاریخ خرید</li>
+                <li className={classes.shoppingCart}>سبد خرید</li>
             </ul>
+            {/* <Cart /> */}
         </div>
         </div>
     )
