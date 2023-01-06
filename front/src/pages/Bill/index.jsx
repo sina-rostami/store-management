36ba9a453bc5197d1d1f3bd0f6f9807215c77a84
soroushazizi <soroushazizi@yahoo.com>
2023-01-06 21:01:02 +0330

    style bill page

--- front/src/pages/Bill/index.jsx
+++ front/src/pages/Bill/index.jsx
@@ -5,15 +5,20 @@ import StaticCart from '../../components/StaticCart/index.jsx'
 import getBill from '../../services/getBill.js'
 import getProducts from '../../services/getProducts.js'
 import dltf from "../../utilities/dltf"
+import { ToastContainer, toast } from 'react-toastify'
+import Grid from '@mui/material/Grid'
+import 'react-toastify/dist/ReactToastify.css'
 
 
-function Bill () {
+
+function Bill (props) {
   const [products, setProducts] = useState([])
   const [bill, setBill] = useState(null)
   const [cartItems, setCartItems] = useState([])
   const navigate = useNavigate()
   const location = useLocation()
   const classes = styles()
+  const { items, totalPrice } = props
 
   const handleCartItems = () => {
     if (bill) {
@@ -51,8 +56,45 @@ function Bill () {
       <h1 className={classes.pageTitle}>فاکتور</h1>
       {bill &&
         (
-          <div>
-            <ul className={classes.topSection}>
+          <div className={classes.cartContainer}>
+            <ToastContainer rtl />
+            <Grid container className={classes.headerContainer}>
+        <Grid className={classes.gridHeader} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+          فروشنده
+        </Grid>
+        <Grid className={classes.gridHeader} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+          خریدار
+        </Grid>
+        <Grid className={classes.gridHeader} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+          شناسه خرید
+        </Grid>
+        <Grid className={classes.gridHeader} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+          تاریخ خرید
+        </Grid>
+        <Grid className={classes.gridHeader} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+          ساعت خرید
+        </Grid>
+      </Grid>
+      <hr />
+      {/* {
+        items.map((item) => (
+          <Grid className={classes.cartItem} container key={bill.seller_name}>
+            <Grid className={classes.gridItem} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+              {bill.customer_name}
+            </Grid>
+            <Grid className={classes.gridItem} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+              {dltf(dltf(bill.id))}
+            </Grid>
+            <Grid className={classes.gridItem} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+              {dltf(Intl.DateTimeFormat('fa', {year: 'numeric', month: '2-digit',day: '2-digit' }).format(bill.date * 1000))}
+            </Grid>
+            <Grid className={classes.gridItem} item xs={2.25} sm={2.25} md={2.25} lg={2.25}>
+              {Intl.DateTimeFormat('fa', {hour: '2-digit', minute: '2-digit',second: '2-digit' }).format(bill.date * 1000)}
+            </Grid>
+          </Grid>
+        ))
+      }     */}
+            {/* <ul className={classes.topSection}>
               <li className={classes.sellerName}>فروشنده: {bill.seller_name}</li>
               <li className={classes.customerName}>خریدار: {bill.customer_name}</li>
               <li className={classes.id}>شناسه خرید: {dltf(bill.id)}</li>
@@ -64,7 +106,7 @@ function Bill () {
                 ساعت خرید:
                 {Intl.DateTimeFormat('fa', {hour: '2-digit', minute: '2-digit',second: '2-digit' }).format(bill.date * 1000)}
               </li>
-            </ul>
+            </ul> */}
             {cartItems.length === bill.products.length && <StaticCart totalPrice={bill.total_price} items={cartItems}/>}
           </div>
         )
