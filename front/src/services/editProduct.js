980a5571d7462e963190f5cc416ca24b5968f3cd
soroushazizi <soroushazizi@yahoo.com>
2023-01-06 18:11:01 +0330

    add product api

--- front/src/services/editProduct.js
+++ front/src/services/editProduct.js
@@ -1,1 +1,19 @@
 import apiService from './apiService'
+
+const editProductById = async ({ id, data }) => {
+    try {
+      const response = await apiService(
+        { endpoint: `/product/${id}`, authTokenNeeded: true, method: 'put', data, isMultiPartData: true },
+      )
+      if (!response.data) return { succeeded: true }
+  
+      const result = { ...response.data, succeeded: true }
+  
+      return result
+    } catch (error) {
+      return (console.error(error), false)
+    }
+  }
+  
+  export default editProductById
+  
