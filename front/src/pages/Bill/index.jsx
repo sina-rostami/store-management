e516da5edc7b35ed867e6e86e61c49822128620d
soroushazizi <soroushazizi@yahoo.com>
2023-01-06 14:51:41 +0330

    add cart to bill

--- front/src/pages/Bill/index.jsx
+++ front/src/pages/Bill/index.jsx
@@ -3,20 +3,42 @@ import styles from "./style";
 import { useNavigate, useLocation } from 'react-router-dom'
 import Cart from '../../components/Cart/index.jsx'
 import getBill from '../../services/getBill.js'
+import getProducts from '../../services/getProducts.js'
 
 
 function Bill (props) {
-    
+    const [products, setProducts] = useState([])
     const [bill, setBill] = useState(null)
     const classes = styles()
     const navigate = useNavigate()
     const location = useLocation()
-    console.log(bill)
+    const [cartItems, setCartItems] = useState([])
+    const handleCartItems = () => {
+        if(bill){
+            bill.products_ids.forEach(productId => {
+                const cartItem = products.find(product => product.id === productId)
+                console.log(cartItem)
+                console.log(cartItems)
+                // console.log([...cartItems,...cartItem])
+                const temp = []
+                temp.push(cartItem)
+                setCartItems([...cartItems,...temp])
+            });
+        }
 
+    }
+    
     useEffect(() => {
-        getBill(location.state.id).then(res => setBill(res))
+        getProducts().then(res => setProducts(res))
+        getBill(location.state.id).then(res => {
+            setBill(res)
+        })
     },[])
-
+    useEffect(() => {
+        if(bill){
+            handleCartItems()
+        }
+    },[bill])
     return (
         <div className={classes.pageHeader}>
             <img src="./asset/images/back.png" alt="بازگشت" title='بازگشت' onClick={() => navigate(-1)} />
@@ -29,7 +51,7 @@ function Bill (props) {
                 <li className={classes.purchaseDate}>تاریخ خرید</li>
                 <li className={classes.shoppingCart}>سبد خرید</li>
             </ul>
-            {/* <Cart /> */}
+        {bill &&  cartItems.length === bill.products_ids.length && <Cart  totalPrice={bill.totalPrice} items={cartItems}/>}
         </div>
         </div>
     )
