06113be7a27ebec9dbf2e6d128b899653ca95999
Alireza <alireza.dolatabadi.jc@gmail.com>
2023-01-28 18:57:40 +0330

    Pagination for seller

--- backend/src/backend_api.py
+++ backend/src/backend_api.py
@@ -321,7 +321,9 @@ def delete_product(current_user, product_id):
 @admin_authorization
 def get_sellers(current_user):
     try:
-        return jsonify(backend.seller_manager.get_all_sellers_as_json()), HTTPStatus.OK
+        page = request.args.get('page') if request.args.get('page') else 1
+        per_page = request.args.get('per_page') if request.args.get('per_page') else 10
+        return jsonify(backend.seller_manager.get_all_sellers_as_json(int(page), int(per_page))), HTTPStatus.OK
     except BadRequest as e:
         return jsonify({'message': f'{e.description}'}), HTTPStatus.BAD_REQUEST
     except Exception as e:
